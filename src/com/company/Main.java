eadf5e92a7137d8c91e5eafdc52d5f24908b7296
Fatal <Dragonishe@yandex.ru>
2020-05-09 16:57:47 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,35 +21,55 @@ public class Main {
 			statement.executeUpdate("CREATE TABLE IF NOT EXISTS books (id MEDIUMINT NOT NULL AUTO_INCREMENT, name CHAR (30) NOT NULL, PRIMARY KEY (id))");
 			statement.executeUpdate("insert into books (name) values ('Dark Tower')");
 			statement.executeUpdate("insert into books (name) values ('Lord of the rings')");
+			statement.executeUpdate("insert into books (name) values ('Harry Potter')");
 
-			CallableStatement callableStatement = connection.prepareCall("{call BooksCount(?)}");
-			callableStatement.registerOutParameter(1, Types.INTEGER);
-			callableStatement.execute();
-			System.out.println(callableStatement.getInt(1));
-			System.out.println("________________");
+			Statement statement1 = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			ResultSet resultSet = statement1.executeQuery("select * from books");
+			if (resultSet.next())
+				System.out.println(resultSet.getString("name"));
+			if (resultSet.next())
+				System.out.println(resultSet.getString("name"));
+			if (resultSet.previous())
+				System.out.println(resultSet.getString("name"));
+			if (resultSet.relative(2))
+				System.out.println(resultSet.getString("name"));
+			if (resultSet.relative(-2))
+				System.out.println(resultSet.getString("name"));
+			if (resultSet.absolute(2))
+				System.out.println(resultSet.getString("name"));
+			if (resultSet.first())
+				System.out.println(resultSet.getString("name"));
+			if (resultSet.last())
+				System.out.println(resultSet.getString("name"));
 
-			CallableStatement callableStatement1 = connection.prepareCall("{call getBooks(?)}");
-			callableStatement1.setInt(1, 1);
-			if (callableStatement1.execute()){
-				ResultSet resultSet = callableStatement1.getResultSet();
-				while (resultSet.next()){
-					System.out.println(resultSet.getInt("id"));
-					System.out.println(resultSet.getString("name"));
-				}
+			System.out.println("_____________________");
+
+
+			Statement statement2 = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			ResultSet resultSet2 = statement2.executeQuery("select * from books");
+			while (resultSet2.next()){
+				System.out.println(resultSet2.getInt("id"));
+				System.out.println(resultSet2.getString("name"));
 			}
 
-			System.out.println("_______________________");
+			System.out.println("________________________");
 
-			CallableStatement callableStatement2 = connection.prepareCall("{call getCount()}");
-			boolean hasResult = callableStatement2.execute();
-			while (hasResult){
-				ResultSet resultSet = callableStatement2.getResultSet();
-				while (resultSet.next()){
-					System.out.println(resultSet.getInt(1));
-				}
-				hasResult = callableStatement2.getMoreResults();
+			resultSet2.last();
+			resultSet2.updateString("name", "Fantastic beasts");
+			resultSet2.updateRow();
+
+			resultSet2.moveToInsertRow();
+			resultSet2.updateString("name", "Hobbit");
+			resultSet2.insertRow();
+
+			resultSet2.absolute(2);
+			resultSet2.deleteRow();
+
+			resultSet2.beforeFirst();
+			while (resultSet2.next()){
+				System.out.println(resultSet2.getInt("id"));
+				System.out.println(resultSet2.getString("name"));
 			}
-
    		 }
     }
 }
